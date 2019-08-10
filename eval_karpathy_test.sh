985383a54c2c8cfdf019c095a16d15786ab4a382
congluwen <congve1@live.com>
2019-08-10 23:26:03 +0800

    add sth

--- eval_karpathy_test.sh
+++ eval_karpathy_test.sh
@@ -17,11 +17,11 @@ fi
 
 export CUDA_VISIBLE_DEVICES=$GPU
 commands=(
-'python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 1 >karpathy_test_results/${id}_model_${iter}.txt 2>&1'
-'python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 2 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1'
-'python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 3 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1'
-'python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 4 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1'
-'python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 5 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1'
+"python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 1 >karpathy_test_results/${id}_model_${iter}.txt 2>&1"
+"python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 2 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1"
+"python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 3 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1"
+"python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 4 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1"
+"python eval.py --dump_images 0 --num_images 5000 --language_eval 1 --model log_${id}/model-${iter}.pth --infos_path log_${id}/infos_${id}-${iter}.pkl --beam_size 5 >>karpathy_test_results/${id}_model_${iter}.txt 2>&1"
 )
 for command in "${commands[@]}"; do
     while true; do
